4d35b33e71a0b7304bfb2fa7fabd785089235381
Adm <81183115+Adm-Tariq@users.noreply.github.com>
2024-06-07 18:38:47 +0300

    Update1 About.js

--- src/screen/About.js
+++ src/screen/About.js
@@ -36,7 +36,7 @@ const About = () => {
                         <div className="row d-grid mt-3 " >
                             <div className="col image">
                                 <img src="https://upcdn.io/12a1yuU/raw/images/html.svg" className="rounded" alt="..." />
-                                <img src="https://files.fm/f/atbb6te4tj" className="rounded" alt="..." />
+                                <img src="https://www.imghippo.com/i/sZy551717774688.svg" className="rounded" alt="..." />
                                 <img src="https://upcdn.io/12a1yuU/raw/images/js.svg" className="rounded" alt="..." />
                                 <img src="https://upcdn.io/12a1yuU/raw/images/react.svg" className="rounded" alt="..." />
                                 <img src="https://upcdn.io/12a1yuU/raw/images/redux.svg" className="rounded" alt="..." />
